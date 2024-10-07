8f9904175151b0d6ed25dc9b0bca7fb4fbe0f1a1
adityap-cyber <129793319+adityap-cyber@users.noreply.github.com>
2024-10-07 14:20:23 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,6 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        # Stack to keep track of opening brackets
+        stack = []
+        
+        # Iterate over each character in the string
+        for char in s:
+            if char in bracket_map:
+                # Pop the top element from the stack if not empty, else assign '#'
+                top_element = stack.pop() if stack else '#'
+                
+                # If the mapping for this bracket doesn't match the stack's top, return False
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                # Push the opening bracket onto the stack
+                stack.append(char)
+        
+        # In the end, if the stack is empty, then we have a valid set of parentheses
+        return not stack
         pass
 
 
